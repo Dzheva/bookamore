070a931264d012742519f14a1ac401d838f705ac
Ihor Dzheva <idzheva@gmail.com>
2025-07-07 08:11:14 +0300

    setup backend project

---
+++ backend/src/main/java/com/booksy/backend/entity/User.java
@@ -0,0 +1,20 @@
+package com.booksy.backend.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "users")
+@Data
+public class User extends BaseEntity{
+    @Column
+    private String name;
+    @Column
+    private String surname;
+    @Column
+    private String email;
+    @Column
+    private String password;
+}
