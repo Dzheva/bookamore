f4cd71a9e1cdb56cc88815c2f9441e5765b42e16
khayise <dedeshdeu@gmail.com>
2025-07-12 21:17:48 +0200

    T-007. Configured a basic RTK store

--- frontend/src/app/main.tsx
+++ frontend/src/app/main.tsx
@@ -2,12 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import {RouterProvider} from "react-router";
 import {router} from "@app/router/router.tsx";
+import {Provider as ReduxProvider} from "react-redux";
 
 import '@app/styles/globals.css'
+import {store} from "@app/store/store.ts";
 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-      <RouterProvider router={router} />
+      <ReduxProvider store={store}>
+          <RouterProvider router={router} />
+      </ReduxProvider>
   </StrictMode>,
 )
