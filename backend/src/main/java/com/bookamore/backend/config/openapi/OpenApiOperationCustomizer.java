ca1f5990e35ac83e5eb3b643ffa182086cc072da
Ihor Dzheva <idzheva@gmail.com>
2025-08-25 18:54:02 +0300

    refactor OpenApiOperationCustomizer simplify code

--- backend/src/main/java/com/bookamore/backend/config/openapi/OpenApiOperationCustomizer.java
+++ backend/src/main/java/com/bookamore/backend/config/openapi/OpenApiOperationCustomizer.java
@@ -9,6 +9,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.method.HandlerMethod;
 
+import java.lang.annotation.Annotation;
+
 @Configuration
 public class OpenApiOperationCustomizer implements OperationCustomizer {
 
@@ -18,54 +20,72 @@ public class OpenApiOperationCustomizer implements OperationCustomizer {
             return operation;
         }
 
-        handeRemoveCodeAnnotations(operation, handlerMethod);
+        handleRemoveCodeAnnotations(operation, handlerMethod);
 
         return operation;
     }
 
-    private void handleNo401SwgrAnnotation(Operation operation, HandlerMethod handlerMethod) {
-        boolean isOnMethod = handlerMethod.getMethod().isAnnotationPresent(No401Swgr.class);
-        boolean isOnClass = handlerMethod.getBeanType().isAnnotationPresent(No401Swgr.class);
+//    private void handleNo401SwgrAnnotation(Operation operation, HandlerMethod handlerMethod) {
+//        boolean isOnMethod = handlerMethod.getMethod().isAnnotationPresent(No401Swgr.class);
+//        boolean isOnClass = handlerMethod.getBeanType().isAnnotationPresent(No401Swgr.class);
+//
+//        if (!isOnMethod && !isOnClass) {
+//            return;
+//        }
+//
+//        operation.getResponses().remove(getCodeOf(HttpStatus.UNAUTHORIZED));
+//    }
+//
+//    private void handleNo404SwgrAnnotation(Operation operation, HandlerMethod handlerMethod) {
+//        boolean isOnMethod = handlerMethod.getMethod().isAnnotationPresent(No404Swgr.class);
+//        boolean isOnClass = handlerMethod.getBeanType().isAnnotationPresent(No404Swgr.class);
+//
+//        if (!isOnMethod && !isOnClass) {
+//            return;
+//        }
+//
+//        operation.getResponses().remove(getCodeOf(HttpStatus.NOT_FOUND));
+//    }
+//
+//    private void handleNo409SwgrAnnotation(Operation operation, HandlerMethod handlerMethod) {
+//        boolean isOnMethod = handlerMethod.getMethod().isAnnotationPresent(No409Swgr.class);
+//        boolean isOnClass = handlerMethod.getBeanType().isAnnotationPresent(No409Swgr.class);
+//
+//        if (!isOnMethod && !isOnClass) {
+//            return;
+//        }
+//
+//        operation.getResponses().remove(getCodeOf(HttpStatus.CONFLICT));
+//    }
+//
+//    private String getCodeOf(HttpStatus status) {
+//        return String.valueOf(status.value());  // returns string http status value (e.g. "404", "409")
+//    }
+//
+//    private void handleRemoveCodeAnnotations(Operation operation, HandlerMethod handlerMethod) {
+//
+//        handleNo401SwgrAnnotation(operation, handlerMethod);
+//
+//        handleNo404SwgrAnnotation(operation, handlerMethod);
+//
+//        handleNo409SwgrAnnotation(operation, handlerMethod);
+//    }
 
-        if (!isOnMethod && !isOnClass) {
-            return;
+    private void removeByAnnotation(Operation operation, HandlerMethod handlerMethod,
+                                    Class<? extends Annotation> annotationClass,
+                                    HttpStatus httpStatus) {
+        boolean isOnMethod = handlerMethod.getMethod().isAnnotationPresent(annotationClass);
+        boolean isOnClass = handlerMethod.getBeanType().isAnnotationPresent(annotationClass);
+
+        if (isOnMethod || isOnClass) {
+            String code = String.valueOf(httpStatus.value());
+            operation.getResponses().remove(code);
         }
-
-        operation.getResponses().remove(getCodeOf(HttpStatus.UNAUTHORIZED));
     }
 
-    private void handleNo404SwgrAnnotation(Operation operation, HandlerMethod handlerMethod) {
-        boolean isOnMethod = handlerMethod.getMethod().isAnnotationPresent(No404Swgr.class);
-        boolean isOnClass = handlerMethod.getBeanType().isAnnotationPresent(No404Swgr.class);
-
-        if (!isOnMethod && !isOnClass) {
-            return;
-        }
-
-        operation.getResponses().remove(getCodeOf(HttpStatus.NOT_FOUND));
-    }
-
-    private void handleNo409SwgrAnnotation(Operation operation, HandlerMethod handlerMethod) {
-        boolean isOnMethod = handlerMethod.getMethod().isAnnotationPresent(No409Swgr.class);
-        boolean isOnClass = handlerMethod.getBeanType().isAnnotationPresent(No409Swgr.class);
-
-        if (!isOnMethod && !isOnClass) {
-            return;
-        }
-
-        operation.getResponses().remove(getCodeOf(HttpStatus.CONFLICT));
-    }
-
-    private String getCodeOf(HttpStatus status) {
-        return String.valueOf(status.value());  // returns string http status value (e.g. "404", "409")
-    }
-
-    private void handeRemoveCodeAnnotations(Operation operation, HandlerMethod handlerMethod) {
-
-        handleNo401SwgrAnnotation(operation, handlerMethod);
-
-        handleNo404SwgrAnnotation(operation, handlerMethod);
-
-        handleNo409SwgrAnnotation(operation, handlerMethod);
+    private void handleRemoveCodeAnnotations(Operation operation, HandlerMethod handlerMethod) {
+        removeByAnnotation(operation, handlerMethod, No401Swgr.class, HttpStatus.UNAUTHORIZED);
+        removeByAnnotation(operation, handlerMethod, No404Swgr.class, HttpStatus.NOT_FOUND);
+        removeByAnnotation(operation, handlerMethod, No409Swgr.class, HttpStatus.CONFLICT);
     }
 }
