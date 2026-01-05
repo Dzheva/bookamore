119338c4df46ffcb7ef0ac2293bbab84b5f71777
Maryna Duka <89296066105marina@gmail.com>
2026-01-05 19:18:23 +0100

    TFF-96. Create Top Back Navigation component

--- frontend/src/pages/ProfilePage/ProfilePage.tsx
+++ frontend/src/pages/ProfilePage/ProfilePage.tsx
@@ -1,26 +1,23 @@
 import { BottomNav } from "@/shared/ui/BottomNav";
-import { IoChevronBack } from "react-icons/io5";
-import { useNavigate } from "react-router-dom";
+import NavBackBtn from "@/shared/ui/NavBackBtn";
 
 const ProfilePage = () => {
-    const navigate = useNavigate()
+  return (
+    <>
+      <NavBackBtn />
+      <div>
+        <h1 className=" text-lg sm:text-xl lg:text-2xl flex justify-center p-[10px]  font-medium text-[20px] leading-[1.25] font-sans  ">
+          Profile
+        </h1>
+      </div>
 
-    const handleBack = () => {
-        navigate(-1)
-    }
-    return (
-        <>
-            <div className="bg-white px-4 sm:px-6 lg:px-8 py-3 flex items-center border-b border-gray-200">
-                <button onClick={handleBack} className="p-1 sm:p-1.5">
-                    <IoChevronBack className="w-6 h-6 text-black" />
-                </button>
-                <h1 className="text-lg sm:text-xl lg:text-2xl font-semibold flex-1 text-center">Edit Profile</h1>
-                <div className="w-8"></div>
-            </div>
-            <h1 className="text-2xl text-center text-slate-600 mt-10 animate-pulse">This page is under development.</h1>
-            <BottomNav isProfilePage={true} />
-        </>
-    );
+      <h1 className="text-2xl text-center text-slate-600 mt-10 animate-pulse">
+        This page is under development.
+      </h1>
+
+      <BottomNav isProfilePage={true} />
+    </>
+  );
 };
 
 export default ProfilePage;
