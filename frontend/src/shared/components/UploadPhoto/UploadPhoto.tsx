9f84433053c83421a847982ebaab716340b6fa7e
Maryna Duka <89296066105marina@gmail.com>
2026-02-17 17:10:05 +0100

    add photo

--- frontend/src/shared/components/UploadPhoto/UploadPhoto.tsx
+++ frontend/src/shared/components/UploadPhoto/UploadPhoto.tsx
@@ -1,98 +1,115 @@
 import { AddPhotoBook } from '@/shared/ui/icons/AddPhotoBookSvg';
 import { NoImgAddPhoto } from '@/shared/ui/icons/NoImgAddSvg';
-import { useState, type ChangeEvent } from 'react';
+import { useEffect, useRef, useState, type ChangeEvent } from 'react';
 
 const UploadPhoto = () => {
-  const [selectPhoto, setSelectPhoto] = useState<string[]>([]);
+  const [previewUrls, setpPreviewUrls] = useState<string[]>([]);
   const [photo, setPhoto] = useState<File[]>([]);
+
+  const url = useRef(photo);
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     if (!e.target.files) return;
 
-    const remaining = 4 - photo.length;
-
-    if (remaining <= 0) return;
-    const photoArr = Array.from(e.target.files).slice(0, remaining);
+    const photoArr = Array.from(e.target.files);
     setPhoto((prev) => [...prev, ...photoArr]);
 
     console.log(photoArr);
     console.log(photo);
 
     const urls = photoArr.map((photo) => URL.createObjectURL(photo));
-    setSelectPhoto((prev) => [...prev, ...urls]);
+    setpPreviewUrls((prev) => [...prev, ...urls]);
   };
+
+  useEffect(() => {
+    return () => {
+      url.current?.forEach((photoUrl) => {
+        URL.revokeObjectURL(photoUrl);
+      });
+    };
+  }, []);
 
   return (
     <>
       <div>
         <h3 className=" text-h3m   p-[8px]">Upload photo</h3>
-        <div className=" flex">
-          <div>
-            <label
-              className="w-[104px] h-[144px]  bg-[#F7F8F2] 
+        {/*  */}
+
+        {photo.length >= 0 && photo.length < 4 ? (
+          <div className="flex  ">
+            <div>
+              <label
+                className="w-[104px] h-[144px]  bg-[#F7F8F2] 
+                  mt-[6px] mr-[16px] mb-[6px] ml-[8px]
+                  flex items-center justify-center
+                 "
+              >
+                <input
+                  id="fileItem"
+                  onChange={handleChange}
+                  type="file"
+                  accept="image/*"
+                  className="hidden"
+                  multiple
+                />
+
+                <AddPhotoBook />
+              </label>
+            </div>
+
+            <div>
+              <ul
+                className="min-w-[191px] h-[156px] 
+                  flex justify-center gap-[12px]  flex-wrap 
+                  px-[33.5px]"
+              >
+                {Array.from({ length: 4 }).map((_, i) => (
+                  <li
+                    key={i}
+                    className="bg-[#F7F8F2] w-[56px] h-[72px] rounded-[10px]
+                       flex items-center justify-center
+                        "
+                  >
+                    {previewUrls[i] ? (
+                      <img src={previewUrls[i]} alt={`photo-${i}`} />
+                    ) : (
+                      <NoImgAddPhoto />
+                    )}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+        ) : (
+          <div className="flex  ">
+            <div
+              className="w-[104px] h-[144px] 
                   mt-[6px] mr-[16px] mb-[6px] ml-[8px]
                   flex items-center justify-center
                  "
             >
-              <input
-                id="fileItem"
-                onChange={handleChange}
-                type="file"
-                accept="image/*"
-                className="hidden"
-                multiple
-              />
-
-              <AddPhotoBook />
-            </label>
-          </div>
-          <div>
+              {previewUrls[0] && <img src={previewUrls[0]} alt={'photo'} />}
+            </div>
             <ul
               className="min-w-[191px] h-[156px] 
-                  flex justify-center gap-[12px]  flex-wrap 
+                  flex justify-center gap-[12px] items-center  flex-wrap 
                   px-[33.5px]
-                  "
+                 
+                 "
             >
-              {photo.length > 0 && photo.length < 4 ? (
-                selectPhoto.map((url, i) => (
-                  <li
-                    key={i}
-                    className="bg-[#F7F8F2] w-[56px] h-[72px] rounded-[10px]
-                        flex items-center justify-center
+              {previewUrls.map((url, i) => (
+                <li
+                  key={i}
+                  className="bg-[#F7F8F2] w-[56px] h-[72px] rounded-[10px]
+                       flex items-center justify-center
                         "
-                  >
-                    <img src={url} alt={`photo-${i}`} />
-                  </li>
-                ))
-              ) : (
-                <NoImgAddPhoto />
-              )}
+                >
+                  <img src={url} alt={`photo-${i}`} />
+                </li>
+              ))}
             </ul>
           </div>
-
-          {/* <div>
-            <ul>
-              {photo.length >= 4 && (
-                <div>
-                  <h1>hi</h1>
-                </div>
-              )}
-              {photo.length >= 4 &&
-                selectPhoto.map((url, i) => (
-                  <li
-                    key={i}
-                    className="bg-[#F7F8F2] w-[56px] h-[72px] rounded-[10px]
-                        flex items-center justify-center
-                        "
-                  >
-                    <img src={url} alt={`photo-${i}`} />
-
-                    <p> {i} </p>
-                  </li>
-                ))}
-            </ul>
-          </div> */}
-        </div>
+        )}
       </div>
     </>
   );
