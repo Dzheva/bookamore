a12d269f18328e484c42abaaf4b01f1cf507784a
Maryna Duka <89296066105marina@gmail.com>
2026-02-17 22:10:53 +0100

    delete photo prev

--- frontend/src/shared/components/UploadPhoto/UploadPhoto.tsx
+++ frontend/src/shared/components/UploadPhoto/UploadPhoto.tsx
@@ -1,41 +1,34 @@
 import { AddPhotoBook } from '@/shared/ui/icons/AddPhotoBookSvg';
 import { NoImgAddPhoto } from '@/shared/ui/icons/NoImgAddSvg';
-import { useEffect, useRef, useState, type ChangeEvent } from 'react';
-
+import { useState, type ChangeEvent } from 'react';
+import { DeletePhotoSvg } from '@/shared/ui/icons/DeletePhotoSvg';
 const UploadPhoto = () => {
-  const [previewUrls, setpPreviewUrls] = useState<string[]>([]);
+  const [previewUrls, setPreviewUrls] = useState<string[]>([]);
   const [photo, setPhoto] = useState<File[]>([]);
-
-  const url = useRef(photo);
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     if (!e.target.files) return;
-
-    const photoArr = Array.from(e.target.files);
+    const limitPhoto = 4 - photo.length;
+    if (limitPhoto <= 0) return;
+    const photoArr = Array.from(e.target.files).slice(0, limitPhoto);
     setPhoto((prev) => [...prev, ...photoArr]);
 
-    console.log(photoArr);
-    console.log(photo);
-
     const urls = photoArr.map((photo) => URL.createObjectURL(photo));
-    setpPreviewUrls((prev) => [...prev, ...urls]);
+    setPreviewUrls((prev) => [...prev, ...urls]);
   };
 
-  useEffect(() => {
-    return () => {
-      url.current?.forEach((photoUrl) => {
-        URL.revokeObjectURL(photoUrl);
-      });
-    };
-  }, []);
+  const handleDelete = (index: number) => {
+    URL.revokeObjectURL(previewUrls[index]);
+    setPreviewUrls((prev) => prev.filter((_, i) => i !== index));
+    setPhoto((prev) => prev.filter((__, i) => i !== index));
+  };
 
   return (
     <>
       <div>
         <h3 className=" text-h3m   p-[8px]">Upload photo</h3>
-        {/*  */}
 
-        {photo.length >= 0 && photo.length < 4 ? (
+        {photo.length < 4 ? (
           <div className="flex  ">
             <div>
               <label
@@ -71,7 +64,19 @@ const UploadPhoto = () => {
                         "
                   >
                     {previewUrls[i] ? (
-                      <img src={previewUrls[i]} alt={`photo-${i}`} />
+                      <div className="relative">
+                        <img src={previewUrls[i]} alt={`photo-${i}`} />
+
+                        <button
+                          className="absolute inset-0 flex items-center justify-center"
+                          onClick={() => {
+                            handleDelete(i);
+                          }}
+                          type="button"
+                        >
+                          <DeletePhotoSvg />
+                        </button>
+                      </div>
                     ) : (
                       <NoImgAddPhoto />
                     )}
@@ -97,14 +102,30 @@ const UploadPhoto = () => {
                  
                  "
             >
-              {previewUrls.map((url, i) => (
+              {previewUrls.map((_, i) => (
                 <li
                   key={i}
                   className="bg-[#F7F8F2] w-[56px] h-[72px] rounded-[10px]
                        flex items-center justify-center
                         "
                 >
-                  <img src={url} alt={`photo-${i}`} />
+                  {previewUrls[i] ? (
+                    <div className="relative">
+                      <img src={previewUrls[i]} alt={`photo-${i}`} />
+
+                      <button
+                        className="absolute inset-0 flex items-center justify-center"
+                        onClick={() => {
+                          handleDelete(i);
+                        }}
+                        type="button"
+                      >
+                        <DeletePhotoSvg />
+                      </button>
+                    </div>
+                  ) : (
+                    <NoImgAddPhoto />
+                  )}
                 </li>
               ))}
             </ul>
@@ -116,56 +137,3 @@ const UploadPhoto = () => {
 };
 
 export default UploadPhoto;
-{
-  /* <div
-                className="flex flex-col 
-              border-[0.4px] border-solid border-[#B6B6B6] rounded-xl 
-              min-h-[201px] 
-              "
-              >
-                <h3 className="font-kyiv text-h3m   p-[8px]">Upload photo</h3>
-    
-                <div className="flex">
-                  <div
-                    className="min-w-[104px] h-[144px]  bg-[#F7F8F2] 
-                  mt-[6px] mr-[16px] mb-[6px] ml-[8px]
-                  flex items-center justify-center
-                  "
-                  >
-                    <label className="">
-                      <input
-                        type="file"
-                        accept="image/*"
-                        multiple
-                        onChange={(e) => handlePhotosChange(e.target.files)}
-                        className="hidden"
-                      />
-    
-                      <AddPhotoBook />
-                    </label>
-                  </div>
-    //  
-                  <div
-                    className="min-w-[191px] h-[156px] 
-                  flex justify-center gap-[12px]  flex-wrap 
-                  px-[33.5px]
-                  "
-                  >
-                    {[1, 2, 3, 4].map((i) => (
-                      <div
-                        key={i}
-                        className="bg-[#F7F8F2] w-[56px] h-[72px] rounded-[10px]
-                        flex items-center justify-center
-                        "
-                      >
-                        <NoImgAddPhoto />
-                      </div>
-                    ))}
-    
-                    {formData.photos.length > 0 && (
-                      <p>{formData.photos.length} photo(s) selected</p>
-                    )}
-                  </div>
-                </div>
-              </div> */
-}
