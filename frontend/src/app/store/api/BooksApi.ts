5b9337cf269d3fcbc2c39095824361cacc11fcf2
Alex Korchenko <mainoceanm@gmail.com>
2026-07-09 16:01:49 +0300

    [TFB-154] - normalize book image objects to path strings

--- frontend/src/app/store/api/BooksApi.ts
+++ frontend/src/app/store/api/BooksApi.ts
@@ -1,5 +1,8 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { extractImagePaths } from '@shared/helpers/extractImagePaths';
 import type { Book, BookPatchRequest } from '@/types/entities/Book';
+
+type RawBook = Omit<Book, 'images'> & { images: Parameters<typeof extractImagePaths>[0] };
 
 export const BooksApi = createApi({
   reducerPath: 'booksApi',
@@ -9,6 +12,10 @@ export const BooksApi = createApi({
   endpoints: (build) => ({
     getBookById: build.query<Book, number | string>({
       query: (id) => `/${id}`,
+      transformResponse: (response: RawBook): Book => ({
+        ...response,
+        images: extractImagePaths(response.images),
+      }),
     }),
     updateBookById: build.mutation<Book, BookPatchRequest>({
       query: (bookPatchRequest) => ({
